e16fec05ee1b1edaca45031b15894217b42ff15c
Pablo Montiel <pablopmontiel98@gmail.com>
2024-09-16 20:33:17 -0300

    nova branch com ajuda

--- src/App.java
+++ src/App.java
@@ -4,6 +4,22 @@ import java.io.IOException;
 
 public class App {
 
+    static String alfabeto = "abcdefghijklmnopqrstuvwxyz";
+
+    public static boolean isLetter(char letra){
+         return alfabeto.indexOf(letra) != -1;
+    }
+
+    public static int letrasContagem(String conteudo) {
+        int cont = 0;
+        for (int i = 0; i < conteudo.length(); i++) {
+            if (isLetter(conteudo.charAt(i))) {
+                cont++;
+            }
+        }
+        return cont;
+    }
+
     public static String lerArquivo(String caminhoArquivo) {
         StringBuilder conteudo = new StringBuilder();
 
@@ -19,13 +35,40 @@ public class App {
         return conteudo.toString();
     }
 
+    public static double getIC(String conteudo){
+        int[] contLetras = new int[26];
+
+        for (int i = 0; i < alfabeto.length(); i++) {
+            int cont = 0;
+            for (int j = 0; j < conteudo.length(); j++) {
+                if(conteudo.charAt(j) == alfabeto.charAt(i)){
+                    cont ++;
+                }
+            }
+            contLetras[i] = cont;
+        }
+
+        double result = 0;
+        for (int i = 0; i < contLetras.length; i++) {
+            int ni = contLetras[i];
+            result += ni * (ni -1);
+        }
+
+        int N = letrasContagem(conteudo);
+        result = result/ (N * (N - 1));
+        return result;
+    }
+
     public static void main(String[] args) {
 
         int letras[] = new int[26];
+        double frequencia[] = new double[26];
 
         String caminho = "20201-teste1.txt";
         String conteudo = lerArquivo(caminho);
-        int n = conteudo.length();
+
+        double total = getIC(conteudo);
+        System.out.println("IC: " + total);
 
         for (int i = 0; i < conteudo.length(); i++) {
             char c = conteudo.charAt(i);
@@ -42,26 +85,28 @@ public class App {
                 System.out.println((char) (i + 'a') + ": " + letras[i]);
             }
         }
-        System.out.println(conteudo.length());
+        System.out.println("Letras totais: " + conteudo.length());
 
+        double somaMedia = 0;
+
+        for (int i = 0; i < letras.length; i++) {
+            double frequenciaLetra = (double) letras[i] / conteudo.length();
+            frequencia[i] = frequenciaLetra;
+            System.out.println((char) (i + 'a') + ": " + frequenciaLetra);
+            somaMedia = somaMedia + frequenciaLetra;
+        }
+
+        double n = conteudo.length();
+        double result1 = 0;
         double ic = 0;
 
-        for (int i = 0; i < 25; i++) {
-            double freqIndividual = conteudo.charAt(i);
-            double result = (freqIndividual * (freqIndividual - 1) / (n * (n - 1)));
-            ic = ic + result;
+        for (int i = 0; i < letras.length; i++) {
+            result1 = Math.abs((letras[i] * (letras[i] - 1))/ (n * (n - 1)));
+            ic = ic + result1;
         }
 
+        System.out.println(result1);
         System.out.println(ic);
 
-        Linguas lingua;
-
-        if (ic == 1.73) {
-            lingua = Linguas.INGLES;
-        } else {
-            lingua = Linguas.PORTUGUES;
-        }
-
-        System.out.println("Língua: " + lingua);
     }
 }
