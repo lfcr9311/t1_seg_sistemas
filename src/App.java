7f6bd06e0baee0ed7cffd835de940f2670faa03a
Pablo Montiel <pablopmontiel98@gmail.com>
2024-09-18 01:28:04 -0300

    tamanho da chave descoberta

--- src/App.java
+++ src/App.java
@@ -1,13 +1,15 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
 
 public class App {
 
     static String alfabeto = "abcdefghijklmnopqrstuvwxyz";
 
-    public static boolean isLetter(char letra){
-         return alfabeto.indexOf(letra) != -1;
+    public static boolean isLetter(char letra) {
+        return alfabeto.indexOf(letra) != -1;
     }
 
     public static int letrasContagem(String conteudo) {
@@ -35,40 +37,24 @@ public class App {
         return conteudo.toString();
     }
 
-    public static double getIC(String conteudo){
-        int[] contLetras = new int[26];
+    public static Map<Integer, StringBuilder> dividirTexto(int p, String conteudo) {
+        Map<Integer, StringBuilder> fragmento = new HashMap<>();
 
-        for (int i = 0; i < alfabeto.length(); i++) {
-            int cont = 0;
-            for (int j = 0; j < conteudo.length(); j++) {
-                if(conteudo.charAt(j) == alfabeto.charAt(i)){
-                    cont ++;
-                }
-            }
-            contLetras[i] = cont;
+        for (int i = 0; i < p; i++) {
+            fragmento.put(i, new StringBuilder());
         }
 
-        double result = 0;
-        for (int i = 0; i < contLetras.length; i++) {
-            int ni = contLetras[i];
-            result += ni * (ni -1);
+        for (int j = 0; j < conteudo.length(); j++) {
+            int index = j % p;
+            fragmento.get(index).append(conteudo.charAt(j));
         }
 
-        int N = letrasContagem(conteudo);
-        result = result/ (N * (N - 1));
-        return result;
+        return fragmento;
     }
 
-    public static void main(String[] args) {
-
+    public static double[] getIC(String conteudo) {
         int letras[] = new int[26];
         double frequencia[] = new double[26];
-
-        String caminho = "20201-teste1.txt";
-        String conteudo = lerArquivo(caminho);
-
-        double total = getIC(conteudo);
-        System.out.println("IC: " + total);
 
         for (int i = 0; i < conteudo.length(); i++) {
             char c = conteudo.charAt(i);
@@ -79,34 +65,51 @@ public class App {
             }
         }
 
-        System.out.println("Letras encontradas no arquivo:");
-        for (int i = 0; i < letras.length; i++) {
-            if (letras[i] > 0) {
-                System.out.println((char) (i + 'a') + ": " + letras[i]);
+        for (int i = 0; i < 26; i++) {
+            frequencia[i] = (double) letras[i] / conteudo.length();
+        }
+
+        return frequencia;
+    }
+
+    public static void main(String[] args) {
+
+        int letras[] = new int[26];
+        double frequencia[] = new double[26];
+
+        String caminho = "20201-teste2.txt";
+        String conteudo = lerArquivo(caminho);
+
+        Map<Integer, StringBuilder> mapa = new HashMap<>();
+
+        mapa = dividirTexto(7, conteudo);
+
+        for (int i = 0; i < mapa.size(); i++) {
+            getIC(mapa.get(i).toString());
+
+            int[] frequenciaLetras = new int[26];
+
+            conteudo = mapa.get(i).toString();
+
+            for (int j = 0; j < conteudo.length(); j++) {
+                char c = conteudo.charAt(j);
+                if (c >= 'a' && c <= 'z') {
+                    frequenciaLetras[c - 'a']++;
+                }
             }
+
+            // System.out.println("Frequência de letras no Fragmento " + i + ":");
+            // for (int k = 0; k < frequenciaLetras.length; k++) {
+            //     System.out.println((char) (k + 'a') + ": " + frequenciaLetras[k]);
+            // }
+
+            long N = conteudo.length();
+            double IC = 0.0;
+            for (int f : frequenciaLetras) {
+                IC += f * (f - 1);
+            }
+            IC /= N * (N - 1);
+            System.out.println("Índice de Coincidência (IC) do Fragmento " + i + ": " + IC);
         }
-        System.out.println("Letras totais: " + conteudo.length());
-
-        double somaMedia = 0;
-
-        for (int i = 0; i < letras.length; i++) {
-            double frequenciaLetra = (double) letras[i] / conteudo.length();
-            frequencia[i] = frequenciaLetra;
-            System.out.println((char) (i + 'a') + ": " + frequenciaLetra);
-            somaMedia = somaMedia + frequenciaLetra;
-        }
-
-        double n = conteudo.length();
-        double result1 = 0;
-        double ic = 0;
-
-        for (int i = 0; i < letras.length; i++) {
-            result1 = Math.abs((letras[i] * (letras[i] - 1))/ (n * (n - 1)));
-            ic = ic + result1;
-        }
-
-        System.out.println(result1);
-        System.out.println(ic);
-
     }
 }
